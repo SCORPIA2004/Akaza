76b3810090791c07d05bd851e4233e7a55271edd
Muhammad Shayan Usman <mshayanalwaha@gmail.com>
2023-08-16 09:54:45 +0300

    Completed boilerplate with main -> wrapper -> home

---
+++ Flutter/brew_crew/lib/screens/wrapper.dart
@@ -0,0 +1,15 @@
+import 'package:brew_crew/screens/authenticateScreen/authenticate.dart';
+import 'package:brew_crew/screens/homeScreen/home.dart';
+import 'package:flutter/material.dart';
+
+class Wrapper extends StatelessWidget {
+  const Wrapper({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+
+    // return either home or authenticate widget, depending on whether user is signed in ot not
+
+    return Authenticate();
+  }
+}
