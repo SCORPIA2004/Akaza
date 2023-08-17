2f3d19cbaf7f361f2a4639554487f54c608e2296
Muhammad Shayan Usman <mshayanalwaha@gmail.com>
2023-08-17 12:05:39 +0300

    Using Provider to get data from auth and give it to all desc. of wrapper

--- Flutter/brew_crew/lib/main.dart
+++ Flutter/brew_crew/lib/main.dart
@@ -1,6 +1,10 @@
+import 'package:brew_crew/models/userClass.dart';
 import 'package:brew_crew/screens/wrapper.dart';
+import 'package:brew_crew/services/auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:provider/provider.dart';
+import 'models/userClass.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -14,8 +18,13 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Wrapper(),
+    return StreamProvider<UserClass?>.value(
+      // StreamProvider listening to userAuthenticationChanged method and wrapping up the MaterialApp, so any descendents of MaterialApp can access the data
+      value: AuthService().userAuthenticationChanged,
+      initialData: null,
+      child: MaterialApp(
+        home: Wrapper(),
+      ),
     );
   }
 }
--- Flutter/brew_crew/lib/screens/wrapper.dart
+++ Flutter/brew_crew/lib/screens/wrapper.dart
@@ -1,7 +1,8 @@
 import 'package:brew_crew/screens/authenticateScreen/authenticate.dart';
-import 'package:brew_crew/screens/homeScreen/home.dart';
-import 'package:brew_crew/services/sign_in.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../models/userClass.dart';
 
 class Wrapper extends StatelessWidget {
   const Wrapper({Key? key}) : super(key: key);
@@ -9,6 +10,8 @@ class Wrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // return either home or authenticate widget, depending on whether user is signed in ot not
+    final userDetails = Provider.of<UserClass?>(context);
+    print(userDetails);
 
     return Authenticate();
   }
