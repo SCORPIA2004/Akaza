4318f65348d5bef9167d8a3e25198f8c51340329
Muhammad Shayan Usman <mshayanalwaha@gmail.com>
2023-09-27 22:22:56 +0300

    Added files

---
+++ WhatsappMessageSender/main.py
@@ -0,0 +1,10 @@
+import pywhatkit
+
+def main():
+    pywhatkit.sendwhatmsg("+905347758076", "Hello", 12, 3, 7, tab_close=True)
+    # pywhatkit.sendwhatmsg_to_group_instantly("test","hello", tab_close=True)
+
+if __name__ == '__main__':
+    main()
+
+
