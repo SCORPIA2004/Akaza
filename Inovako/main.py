f2c6e33615cf5375c0533ec56547358ddea736c4
Muhammad Shayan Usman <mshayanalwaha@gmail.com>
2023-09-28 14:23:02 +0300

    training

--- Inovako/main.py
+++ Inovako/main.py
@@ -7,5 +7,5 @@ model = YOLO("yolov8n.yaml")  # build a new model from scratch
 # Use the model
 model.train(data="config.yaml", epochs=3)  # train the model
 metrics = model.val()  # evaluate model performance on the validation set
-results = model("C:\Users\mshay\Shayan\Portfolio\Projects\Akaza\Inovako\dataset\test\baja.jpg")  # predict on an image
+results = model("test\baja.jpg")  # predict on an image
 path = model.export(format="onnx")  # export the model to ONNX format
