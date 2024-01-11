f00fc45012a08f532ee7c33d227b615696ca2957
Muhammad Shayan Usman <mshayanalwaha@gmail.com>
2024-01-11 19:51:02 +0300

    Added camera control and fixed movement controls

--- GodotPrep/hello world/Player.gd
+++ GodotPrep/hello world/Player.gd
@@ -1,13 +1,43 @@
 extends RigidBody3D
 
+var mouse_sensitivity := 0.001
+var twist_input := 0.0
+var pitch_input := 0.0
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	var inputForwBack = Input.get_action_strength("ui_up") - Input.get_action_strength("ui_down")
-	#var inputDown = Input.get_action_strength("ui_down")
-	apply_central_force(inputForwBack * Vector3.FORWARD * 1200.0 * delta)
+	var rate := 2000.0
+	var inputMove := Vector3.ZERO
+	inputMove.x = Input.get_axis("move_left", "move_right")
+	inputMove.z = Input.get_axis("move_forward", "move_backward")
+	
+	apply_central_force(inputMove * rate * delta)
+	
+	if Input.is_action_just_pressed("ui_cancel"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		
+	$VertPivot.rotate_y(twist_input)
+	$VertPivot/HoriPivot.rotate_x(pitch_input)
+	$VertPivot/HoriPivot.rotation.x = clamp(
+		$VertPivot/HoriPivot.rotation.x,
+		-0.5,
+		0.5
+	)
+	twist_input = 0.0
+	pitch_input = 0.0
+	
+	
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+			twist_input = - event.relative.x * mouse_sensitivity
+			pitch_input = - event.relative.y * mouse_sensitivity
+			
+	
