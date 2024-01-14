ab1db1cef563e7a6ad71ce5dc96c3bef6217900d
Muhammad Shayan Usman <mshayanalwaha@gmail.com>
2024-01-14 22:54:06 +0300

    Using useState hooks

--- ReactFirebasePrep/helloWorld/hello-world/src/App.js
+++ ReactFirebasePrep/helloWorld/hello-world/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [name, setName] = useState("mario")
+
+  const handleOnClick = () => {
+    if (name === "mario")
+      setName("luigi")
+    else
+      setName("mario")
+    console.log(name)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>My name is {name}</h2>
+      <button onClick={handleOnClick}>Change name</button>
     </div>
   );
 }
